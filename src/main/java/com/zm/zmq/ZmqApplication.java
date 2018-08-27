0160f1d07216ca7cc992c5f72ab65af97b7e4b40
zm <270417599@qq.com>
2018-08-27 22:21:34 +0800

    add framework

---
+++ src/main/java/com/zm/zmq/ZmqApplication.java
@@ -0,0 +1,12 @@
+package com.zm.zmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZmqApplication.class, args);
+    }
+}
